1a9657f9bcecef02d287a76a6c497ec4c4f33fa4
Amir. A <akhavan12@yahoo.com>
2020-03-09 11:32:18 -0400

    update

--- corona_dashboard/backend/html_gen.py
+++ corona_dashboard/backend/html_gen.py
@@ -1,13 +1,21 @@
 import glob 
-
-list_of_countries=sorted(glob.glob("./countries/*/"))
+import os 
+os.chdir('/Users/mac/Documents/GitHub/topojson/')
+list_of_countries=sorted(glob.glob('/Users/mac/Documents/GitHub/topojson/countries/*/'))
 str_html='<select id="countries">'
 
 
+
+
 for fl in list_of_countries:
-    fl=fl.replace("./countries/","")[:-1]
-    fl=f'<option value="{fl}">{fl}</option>'
-    str_html=str_html+fl+'\n'
-str_html=str_html+'</select>'
-with open('html_code.html','w') as fl:
-    fl.write(str_html)
+    files=sorted(glob.glob(fl+'*.json'))
+    os.rename(files[0],'province_state.json')
+
+    print(files)
+    #print(files)
+    #fl=fl.replace("./countries/","")[:-1]
+    #fl=f'<option value="{fl}">{fl}</option>'
+    #str_html=str_html+fl+'\n'
+#str_html=str_html+'</select>'
+#with open('html_code.html','w') as fl:
+#    fl.write(str_html)
