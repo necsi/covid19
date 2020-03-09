cf2cff840db7b3983f98038663a89b8fd7c41772
Amir. A <akhavan12@yahoo.com>
2020-03-09 12:08:40 -0400

    all_files

--- corona_dashboard/backend/html_gen.py
+++ corona_dashboard/backend/html_gen.py
@@ -8,10 +8,28 @@ str_html='<select id="countries">'
 
 
 for fl in list_of_countries:
+    
     files=sorted(glob.glob(fl+'*.json'))
-    os.rename(files[0],'province_state.json')
+    files_spl=files[0].split('/')
+    s="/"
+    s=s.join(files_spl[:-1])
+    s=s+"/topo.json"
+    os.rename(files[0],s)
 
-    print(files)
+
+
+
+
+
+
+    #
+    #try:
+    #    a=1
+    #    #print(files[0])
+    #except Exception as e:
+    #    print(e)
+    #os.rename(files[0],files[0]'province_state.json')
+
     #print(files)
     #fl=fl.replace("./countries/","")[:-1]
     #fl=f'<option value="{fl}">{fl}</option>'
